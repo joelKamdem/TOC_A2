7f1c9af04f50788ca26bba85a02db73c70f2585b
Joel Kamdem <kamdem16@gmail.com>
2018-10-29 12:49:54 -0400

    FEATURES: Binary PDA implemented.
    BinaryPDA class.
    with the method 'isBinaryPda' that returns True if the string given is a a binary Pda with a series of 0s followed by a series of 1s and false otherwise.
    
    Signed-off-by: Joel Kamdem <kamdem16@gmail.com>

--- src/models/BinaryPda.java
+++ src/models/BinaryPda.java
@@ -1,4 +1,24 @@
 package models;
 
+import java.security.KeyStore;
+import java.util.Scanner;
+
 public class BinaryPda {
+
+    public static boolean isBinaryPda(String pda){
+
+        String pdaPattern = "0+1+";
+
+        return  pda.matches(pdaPattern);
+    }
+  /*  public static void main(String args[]){
+
+
+
+        System.out.println("Enter a Palindrome String to be checked with the PDA");
+        Scanner scan = new Scanner(System.in);
+        String pda = scan.nextLine();
+
+        System.out.println(isBinaryPda(pda));
+    }*/
 }
