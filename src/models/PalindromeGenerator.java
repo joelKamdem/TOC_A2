7b0bdc911fdf620adf1f9f25aa1b462f9a62fbd6
Karl Kevin Tiba Fossoh <kevin_tiba@Karls-MBP.hsd1.ga.comcast.net>
2018-11-04 18:20:59 -0500

    palindrome generator complete

--- src/models/PalindromeGenerator.java
+++ src/models/PalindromeGenerator.java
@@ -1,4 +1,21 @@
 package models;
 
 public class PalindromeGenerator {
+
+    // The goal of this class is to take a text element
+    // Then generate a palindrome of that word
+    // In the case the word is already a palindrome, the word is accepted as is
+    // Our tactic is to progressively mirror our input string
+    // Until we get a palindrome
+
+    public static String makePalindrome(String base){
+        String pref = "";
+        int i = base.length() - 1;
+        while(! Palindrome.isBinaryPda(pref + base)){
+            pref = pref + base.charAt(i);
+            i --;
+        }
+        return pref + base;
+    }
+
 }
