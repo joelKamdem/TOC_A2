9f094408387f01aeeb2071a8b37835ff2c1afc27
Karl Kevin Tiba Fossoh <kevin_tiba@Karls-MBP.hsd1.ga.comcast.net>
2018-11-04 19:16:13 -0500

    Modified Stack definition

--- src/models/PalindromeGenerator.java
+++ src/models/PalindromeGenerator.java
@@ -11,12 +11,15 @@ public class PalindromeGenerator {
     public static String makePalindrome(String base){
         String pref = "";
         int i = base.length() - 1;
-        Stack<Character> stack = new Stack<Character>();
-        while(! Palindrome.isPalindrome(stack, pref + base)){
+        while(! Palindrome.isPalindrome(new Stack<Character>(), pref + base)){
             pref = pref + base.charAt(i);
             i --;
         }
         return pref + base;
+    }
+
+    public static void main(String[]args){
+        System.out.println(PalindromeGenerator.makePalindrome("acbbca"));
     }
 
 }
